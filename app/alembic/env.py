a058efcd1d4bd3abafb569d45e8891e16d0f780d
Godwin peter. O <me@godwin.dev>
2025-02-25 14:57:00 +0100

    build: added alembic for migrations

---
+++ app/alembic/env.py
@@ -0,0 +1,71 @@
+# Copyright (c) 2025 <Godwin peter. O>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#  Project: pepper-local
+#  Author: Godwin peter. O (me@godwin.dev)
+#  Created At: Tue 25 Feb 2025 14:51:59
+#  Modified By: Godwin peter. O (me@godwin.dev)
+#  Modified At: Tue 25 Feb 2025 14:51:59
+
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+from alembic import context
+from app.models import Base
+
+config = context.config
+
+if config.config_file_name is not None:
+	fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline() -> None:
+	url = config.get_main_option('sqlalchemy.url')
+	context.configure(
+		url=url,
+		target_metadata=target_metadata,
+		literal_binds=True,
+		dialect_opts={'paramstyle': 'named'},
+	)
+
+	with context.begin_transaction():
+		context.run_migrations()
+
+
+def run_migrations_online() -> None:
+	connectable = engine_from_config(
+		config.get_section(config.config_ini_section, {}),
+		prefix='sqlalchemy.',
+		poolclass=pool.NullPool,
+	)
+
+	with connectable.connect() as connection:
+		context.configure(connection=connection, target_metadata=target_metadata)
+
+		with context.begin_transaction():
+			context.run_migrations()
+
+
+if context.is_offline_mode():
+	run_migrations_offline()
+else:
+	run_migrations_online()
