e852f66fa64ecbe8bc265af3e3ea7730f464d9bd
Godwin peter .O <me@peter.com.ng>
2026-08-05 11:47:11 +0100

    feat: added cached data access to routes

--- pepper-sync/src/pepper/routes/devices.py
+++ pepper-sync/src/pepper/routes/devices.py
@@ -3,13 +3,14 @@ import uuid
 from typing import List
 from fastapi.routing import APIRouter
 from pepper.models import Device
+from pepper import cache_device
 
 router = APIRouter(prefix="/api/devices", tags=["devices"])
 
 
-@router.get("", response_model=list[Device])
+@router.get("", response_model=list)
 async def get_devices(search: str | None = None, page: int = 1, limit: int = 1000):
-    return []
+    return cache_device.get_all()
 
 
 @router.get("/{id:path}", response_model=Device)
--- pepper-sync/src/pepper/routes/positions.py
+++ pepper-sync/src/pepper/routes/positions.py
@@ -1,5 +1,6 @@
 from typing import List
 import uuid
+from pepper import cache_position
 
 from fastapi.routing import APIRouter
 
@@ -8,9 +9,10 @@ from pepper.models.positions import Position
 router = APIRouter(prefix="/api/positions", tags=["positions"])
 
 
-@router.get("", response_model=list[Position])
+@router.get("", response_model=list)
 async def get_positions(search: str | None = None, page: int = 1, limit: int = 1000):
-    return {"message": "positions"}
+    return cache_position.get_all()
+    # return {"message": "positions"}
 
 
 @router.get("/{id:path}", response_model=Position)
